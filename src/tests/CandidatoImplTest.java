1e174dbf4946d0cc4fbdc81c6ad9abce7ad72cac
MatheusADStudy <matheus.ribeiro.tads@gmail.com>
2022-10-07 09:50:50 -0400

    docs(tests for candidatoImpl): added tests

---
+++ src/tests/CandidatoImplTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.rmi.RemoteException;
+import implementacoes.CandidatoImpl;
+
+public class CandidatoImplTest {
+  private CandidatoImpl candidate;
+
+  @Before
+  public void setup() throws RemoteException {
+    candidate = new CandidatoImpl(1, "fulano");
+  }
+
+  @Test
+  public void testNewCandidatoName() {
+    assertEquals("fulano", candidate.getNome());
+  }
+
+  @Test
+  public void testNewCandidatoNumber() {
+    assertEquals(1, candidate.getNumero());
+  }
+}
