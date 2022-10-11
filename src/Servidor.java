cf3025a622ffd7484bbc4d967acde93e6f366be8
MatheusADStudy <matheus.ribeiro.tads@gmail.com>
2022-10-11 09:28:12 -0400

    refact(client & server): removed some records, added missing actions

--- src/Servidor.java
+++ src/Servidor.java
@@ -15,31 +15,23 @@ public class Servidor {
 	static int porta = 1099;
     
 	public static void main(String[] args) {
-        try {
-        	//IsEvenImpl x = new IsEvenImpl();
-            String objName = "rmi://localhost:" + porta + "/server";
-			
-            System.out.println("Registrando o objeto no RMIRegistry...");
+		try {
+			String objName = "rmi://localhost:" + porta + "/server";
+
+			System.out.println("Registrando o objeto no RMIRegistry...");
 			LocateRegistry.createRegistry(porta);
-            
-			// Lista de Candidatos
+
 			candidatos = new ArrayList<>();
-			candidatos.add(new CandidatoImpl(1, "1 Luiz Picolo"));
-			candidatos.add(new CandidatoImpl(2, "2 Luiz Picolo"));
-			candidatos.add(new CandidatoImpl(3, "3 Luiz Picolo"));
-			candidatos.add(new CandidatoImpl(4, "4 Luiz Picolo"));
-			candidatos.add(new CandidatoImpl(5, "5 Luiz Picolo"));
-			candidatos.add(new CandidatoImpl(6, "6 Luiz Picolo"));
-			candidatos.add(new CandidatoImpl(7, "7 Luiz Picolo"));
+			candidatos.add(new CandidatoImpl(1, "Fulano da Silva"));
+			candidatos.add(new CandidatoImpl(2, "Beltrano de Talaho"));
 			
 			votacaoImpl = new VotacaoImpl(candidatos);
 			
 			Naming.rebind(objName, votacaoImpl);
-			//Naming.rebind(objName, x);
 			
-            System.out.println("Aguardando Clientes na porta " + porta + "!");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    } 
+			System.out.println("Aguardando Clientes na porta " + porta + "!");
+		} catch (Exception e) {
+				e.printStackTrace();
+		}
+	} 
 }
